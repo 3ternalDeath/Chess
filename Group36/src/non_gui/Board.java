f3a117679dca016d0b6c810deb4d2497c7d5ce55
parva-thaker <parva-thaker@Parvas-Epikness>
2017-01-25 20:20:40 -0700

    ill work on this

---
+++ Group36/src/non_gui/Board.java
@@ -0,0 +1,25 @@
+package non_gui;
+
+public class Board {
+	
+	private Piece[][] map;
+	
+	public Board(){
+		
+		Object[] BKing = {PieceType.K, PieceColour.BLACK};
+		
+		/*
+		map = {
+				{ 'R', 'P', ' ', ' ', ' ', ' ', 'P', 'R' },
+				{ 'N', 'P', ' ', ' ', ' ', ' ', 'P', 'N' },
+				{ 'B', 'P', ' ', ' ', ' ', ' ', 'P', 'B' },
+				{ 'Q', 'P', ' ', ' ', ' ', ' ', 'P', 'Q' },
+				{ 'K', 'P', ' ', ' ', ' ', ' ', 'P', 'K' },
+				{ 'B', 'P', ' ', ' ', ' ', ' ', 'P', 'B' },
+				{ 'N', 'P', ' ', ' ', ' ', ' ', 'P', 'N' },
+				{ 'R', 'P', ' ', ' ', ' ', ' ', 'P', 'R' }
+			};
+			*/
+	}
+
+}
