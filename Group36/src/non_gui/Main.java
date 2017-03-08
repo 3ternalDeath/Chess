972af9a7f5147e9fa640924e19eea5b4460c5ac8
dominic.demierre <dominic.demierre@cs.ucalgary.ca>
2017-03-08 11:29:58 -0700

    removing extraneous code from Main

--- Group36/src/non_gui/Main.java
+++ Group36/src/non_gui/Main.java
@@ -1,39 +1,9 @@
 package non_gui;
 
 public class Main {
-	
-	private static int currentTurn;
-	private static Player p1;
-	private static Player p2;
-
 	public static void main(String[] args) {
-		currentTurn = 1;
-		p1 = new Player(PieceColor.White, 0);
-		p2 = new Player(PieceColor.Black, 0);
-		//create a board populated with the traditional chess arrangement
-		Board board = new Board();
-		board.populateBoard();
+		Game game = new Game(GameType.MULTI);
 		
-		//displays board then updates board according to user input
-
-		while (!board.blackLose() && !board.whiteLose()) {
-			board.display();
-			if (currentTurn % 2 == 1) {
-				System.out.println(p1.getColor());
-				p1.makeMove(board);
-			}
-			else {
-				System.out.println(p2.getColor());
-				p2.makeMove(board);
-			}
-			currentTurn++;
-		}
-		board.display();
-		if(board.blackLose()){
-			System.out.println("White has won, Black has lost");
-		}
-		else if(board.whiteLose()){
-			System.out.println("Black has won, White has lost");
-		}
+		game.run();
 	}
 }
